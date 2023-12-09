02aaface801bb183ba1cfe7079d0dd021bfcd7ab
kyeoungchan <wkc1002@naver.com>
2023-12-09 15:14:40 +0900

    test: Retry judges if continues

--- src/test/java/baseball/vo/RetryTest.java
+++ src/test/java/baseball/vo/RetryTest.java
@@ -1,4 +1,20 @@
-import static org.junit.jupiter.api.Assertions.*;
+package baseball.vo;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 class RetryTest {
-  
+
+    @ParameterizedTest
+    @DisplayName("입력받은 코드에 따라서 계속할지 게임을 멈출지 판단한다.")
+    @CsvSource(value = {"1, true", "2, false"})
+    void kidsOf(int code, boolean result) {
+        Arrays.stream(Retry.values())
+                .filter(r -> r.kidsOf(code))
+                .forEach(r -> assertThat(r.ifContinue()).isEqualTo(result));
+    }
 }
